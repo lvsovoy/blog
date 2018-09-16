e0786a3397d2c8f5ebfe44ed350aea82f2668440
Konstantins Lesovojs <k.lesovoj@gmail.com>
2018-09-16 10:52:49 +0300

    Added basic controllers, security configurations, hello world landing page

--- src/main/java/me/lesovoy/blog/BlogApplication.java
+++ src/main/java/me/lesovoy/blog/BlogApplication.java
@@ -2,11 +2,35 @@ package me.lesovoy.blog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+
 
 @SpringBootApplication
 public class BlogApplication {
 
-	public static void main(String[] args) {
+    private static final String DB_DRIVER = "org.h2.Driver";
+    private static final String DB_CONNECTION = "jdbc:h2:~/posts";
+    private static final String DB_USER = "";
+    private static final String DB_PASSWORD = "";
+
+
+    public static void main(String[] args) {
+
 		SpringApplication.run(BlogApplication.class, args);
+
+
 	}
+
 }
+
+
+
+
