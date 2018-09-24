b1cd3ae55ca27d6d07948296719b9848cc976f3f
Konstantins Lesovojs <k.lesovoj@gmail.com>
2018-09-24 14:20:02 +0300

    Populated database with entries, created query to get them and implemented fragments to display them on home page

--- src/main/java/me/lesovoy/blog/BlogController.java
+++ src/main/java/me/lesovoy/blog/BlogController.java
@@ -5,13 +5,35 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.LinkedList;
+import java.util.List;
+
 @Controller
 public class BlogController {
-    @Value("${spring.application.name}") String appName;
+    @Value("${spring.application.name}")
+    String appName;
 
     @GetMapping("/")
-    public String homePage (Model model){
+    public String homePage(Model model) throws Exception {
+
+        Connection conn = DriverManager.getConnection("jdbc:h2:~/posts","","");
+
+
+        List<Post> postList = new LinkedList<>();
+        Statement stmt = conn.createStatement();
+        ResultSet rs = stmt.executeQuery("SELECT * FROM PUBLIC.POSTS ORDER BY ID DESC");
+        while(rs.next()){
+            postList.add(new Post(rs.getInt("ID"),rs.getTimestamp("DATE"),rs.getString("TEXT")));
+        }
+//        postList.add(new Post(001,"Loreum ipsum sic amet dolortLoreum ipsum sic amet dolortLoreum ipsum sic amet dolortLoreum ipsum sic amet dolortLoreum ipsum sic amet dolortLoreum ipsum sic amet dolortLoreum ipsum sic amet dolortLoreum ipsum sic amet dolortLoreum ipsum sic amet dolortLoreum ipsum sic amet dolortLoreum ipsum sic amet dolortLoreum ipsum sic amet dolortLoreum ipsum sic amet dolortLoreum ipsum sic amet dolortLoreum ipsum sic amet dolortLoreum ipsum sic amet dolort"));
+//        postList.add(new Post(002,"Test2"));
         model.addAttribute("appname", appName);
+        model.addAttribute("posts",postList);
+
         return "home";
     }
 }
---
+++ src/main/java/me/lesovoy/blog/Post.java
@@ -0,0 +1,19 @@
+package me.lesovoy.blog;
+
+import java.io.Serializable;
+import java.sql.Time;
+import java.sql.Timestamp;
+import java.util.Date;
+
+public class Post implements Serializable {
+    public int id;
+    public String text;
+    public Timestamp date;
+
+
+    Post(int id, Timestamp date, String text){
+        this.id = id;
+        this.date = date;
+        this.text = text;
+    }
+}
